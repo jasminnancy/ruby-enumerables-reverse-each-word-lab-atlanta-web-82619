6386d8a06d1c4673052e0b1f4e8a0ff7b67dc512
Alexandria Pugia <alex.pugia816@hotmail.com>
2019-08-19 02:15:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   reverse = string.split(" ")
-  reverse.each do |x|
-    x.reverse
+  reverse.each do |word|
+    word.reverse
   end
   return reverse.join(" ")
 end
